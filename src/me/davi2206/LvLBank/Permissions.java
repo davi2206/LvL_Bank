8e05afe31a50746557bc1cfe90d40756fc898bc9
David Kolind <dmkolind@nokgodtnok.dk>
2015-11-09 19:51:54 +0100

    Added permission

--- src/me/davi2206/LvLBank/Permissions.java
+++ src/me/davi2206/LvLBank/Permissions.java
@@ -6,6 +6,7 @@ public class Permissions
 {
 	public Permission lvlBankReload = new Permission("LvL.reload");
 	public Permission lvlBankOther = new Permission("LvL.other");
+	public Permission lvlBankWorld = new Permission("LvL.world");
 	public Permission lvlBankSignPlace = new Permission("LvL.signPlace");
 	public Permission lvlBankSignBreak = new Permission("LvL.signBreak");
 	public Permission lvlBankSignUse = new Permission("LvL.signUse");
